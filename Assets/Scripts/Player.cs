d9cebe0cfe20d842f608a0f6cc1e9f5f4bacc750
njacke <75493946+njacke@users.noreply.github.com>
2020-12-27 15:59:21 +0100

    v0.0.1
    - Added tile and player sprite sheets
    - Added tilemaps
    - Created tile rules
    - Created player idle, run, and climb animations and transitions
    - Implemented collision for foreground  tiles and player
    - Created player script with movement, sprite flip, run transition bool flip

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // config
+    [SerializeField] float runSpeed = 5f;
+
+    // state
+    bool isAlive = true;
+
+    // cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+
+    // message then methods
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        {
+            myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+        }
+
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1); // Matf.Sign returns -1 or +1. Setting scale to -1 on x if it is minus.
+        }
+    }
+}
